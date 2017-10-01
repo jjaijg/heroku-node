c8c986473d6213a075dee9ce4bca7a7556411daa
Gopinath J <Gopinath J>
2017-10-01 22:58:02 +0530

    added cloud mongodb uri

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    'url': 'mongodb://localhost/testdb'
+    'url': 'mongodb://ue3wtndwpec0p2d:xPMXx4ITlIMVMZiE8jJO@bfx84cvuu0qzuko-mongodb.services.clever-cloud.com:27017/bfx84cvuu0qzuko'
 }
