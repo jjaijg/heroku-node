c5a658d2030b765c1112c9db5c843d585e38336c
Gopinath J <Gopinath J>
2017-10-01 23:08:00 +0530

    config url for local and online development in auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,22 +1,14 @@
+if (process.env.PORT)
+    var url = "http://app-33533789-7d8b-410f-912e-02fea735c626.cleverapps.io/";
+else
+    var url = "http://localhost:3000";
+
 // expose our config directly to our application using module.exports
 module.exports = {
 
     'facebookAuth': {
         'clientID': '315601195579859', // your App ID
         'clientSecret': '146374d491fe29f895ba61fc24ddd8d6', // your App Secret
-        'callbackURL': 'http://localhost:3000/auth/facebook/callback'
-    },
-
-    'twitterAuth': {
-        'consumerKey': 'Q0IApp7qFblu8kZXzjOrQsatc ',
-        'consumerSecret': 'f1dFbl2MoL0dSri3LbRDFNJf2GyifiJp8jiyUqYM63i0Fw2h2F',
-        'callbackURL': 'http://127.0.0.1:3000/auth/twitter/callback'
-    },
-
-    'googleAuth': {
-        'clientID': 'your-secret-clientID-here',
-        'clientSecret': 'your-client-secret-here',
-        'callbackURL': 'http://localhost:8080/auth/google/callback'
+        'callbackURL': url + '/auth/facebook/callback'
     }
-
 };
